389073c021a02a0845067e7ff4d9edecb7ba164d
Wilson Rivera <mr.wilson.rivera@gmail.com>
2022-10-02 16:13:29 -0400

    implemented a custom inspector for the `ObjectController`

---
+++ Assets/Scripts/Editor/ObjectControllerInspector.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DefaultNamespace.Editor
+{
+
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(ObjectController))]
+    public class ObjectControllerInspector : UnityEditor.Editor
+    {
+
+        private SavedBool _showState;
+        private readonly GUIContent sShowStateLabel = new("State");
+
+        private void OnEnable()
+        {
+            _showState = new SavedBool($"ObjectController.{target.GetInstanceID()}.{nameof(_showState)}", false);
+        }
+
+        /// <inheritdoc />
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            EditorGUILayout.Space();
+            var controller = (ObjectController)target;
+
+            _showState.value = EditorGUILayout.Foldout((bool)_showState, sShowStateLabel, true);
+            if (_showState.value)
+            {
+                EditorGUI.BeginDisabledGroup(true);
+                EditorGUILayout.LabelField("Grounded", controller.State.IsGrounded.ToString());
+                EditorGUILayout.LabelField("Ceilinged", controller.State.IsCeilinged.ToString());
+                EditorGUILayout.LabelField("Falling", controller.State.IsFalling.ToString());
+
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Colliding Left", controller.State.IsCollidingLeft.ToString());
+                EditorGUILayout.LabelField("Colliding Right", controller.State.IsCollidingRight.ToString());
+                EditorGUILayout.LabelField("Colliding Above", controller.State.IsCollidingAbove.ToString());
+                EditorGUILayout.LabelField("Colliding Below", controller.State.IsGrounded.ToString());
+
+                EditorGUILayout.Space();
+                EditorGUILayout.ObjectField("Standing On", controller.StandingOn, typeof(GameObject), true);
+                EditorGUILayout.ObjectField("Current Wall", controller.CurrentWall, typeof(GameObject), true);
+
+                EditorGUI.EndDisabledGroup();
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Editor/SavedBool.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+
+namespace DefaultNamespace.Editor
+{
+
+    public sealed class SavedBool
+    {
+
+        private readonly string _name;
+        private bool _isLoaded;
+        private bool _value;
+
+        public SavedBool(string name, bool value)
+        {
+            _name = name;
+            _isLoaded = false;
+            _value = value;
+        }
+
+        public bool value
+        {
+            get
+            {
+                Load();
+                return _value;
+            }
+            set
+            {
+                Load();
+                if (_value == value)
+                    return;
+
+                _value = value;
+                EditorPrefs.SetBool(_name, value);
+            }
+        }
+
+        private void Load()
+        {
+            if (_isLoaded)
+                return;
+
+            _isLoaded = true;
+            _value = EditorPrefs.GetBool(_name, _value);
+        }
+
+        public static implicit operator bool(SavedBool s)
+        {
+            return s.value;
+        }
+
+    }
+
+}
