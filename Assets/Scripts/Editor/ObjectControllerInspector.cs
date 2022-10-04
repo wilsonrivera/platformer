5b5b9cf586a3554be8b97788dcf06569839167f2
Wilson Rivera <mr.wilson.rivera@gmail.com>
2022-10-04 16:36:56 -0400

    added a button to move the `ObjectController` to the closest ground

--- Assets/Scripts/Editor/ObjectControllerInspector.cs
+++ Assets/Scripts/Editor/ObjectControllerInspector.cs
@@ -13,9 +13,18 @@ namespace DefaultNamespace.Editor
         private SavedBool _showState;
         private readonly GUIContent sShowStateLabel = new("State");
 
+        private SerializedProperty _skinWidthProp;
+        private SerializedProperty _numberOfVerticalRaysProp;
+        private SerializedProperty _collisionMaskProp;
+        private SerializedProperty _oneWayPlatformMaskProp;
+
         private void OnEnable()
         {
             _showState = new SavedBool($"ObjectController.{target.GetInstanceID()}.{nameof(_showState)}", false);
+            _skinWidthProp = serializedObject.FindProperty("skinWidth");
+            _numberOfVerticalRaysProp = serializedObject.FindProperty("numberOfVerticalRays");
+            _collisionMaskProp = serializedObject.FindProperty("collisionMask");
+            _oneWayPlatformMaskProp = serializedObject.FindProperty("oneWayPlatformMask");
         }
 
         /// <inheritdoc />
@@ -50,6 +59,48 @@ namespace DefaultNamespace.Editor
                 EditorGUILayout.ObjectField("Current Wall", controller.CurrentWall, typeof(GameObject), true);
 
                 EditorGUI.EndDisabledGroup();
+            }
+
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Move to ground", GUILayout.Height(24)))
+            {
+                MoveToGround(controller);
+            }
+        }
+
+        private void MoveToGround(ObjectController controller)
+        {
+            if (Application.isPlaying)
+            {
+                // If the application is playing, instead of trying to calculate the distance ourself, we'll use the
+                // `WarpToGround` method, which should accomplish the same function, except can only be used
+                // at runtime
+                controller.WarpToGround();
+                return;
+            }
+
+            var collider = controller.gameObject.GetComponent<BoxCollider2D>();
+            var skinWidth = _skinWidthProp.floatValue;
+            var numberOfVerticalRays = _numberOfVerticalRaysProp.intValue;
+            var collisionMask = _collisionMaskProp.intValue | _oneWayPlatformMaskProp.intValue;
+
+            var bounds = collider.bounds;
+            bounds.Expand(-2f * skinWidth);
+            var bottomLeft = bounds.min;
+            var bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+
+            var position = controller.transform.position;
+            for (var i = 0; i < numberOfVerticalRays; i++)
+            {
+                var rayOrigin = Vector2.Lerp(bottomLeft, bottomRight, i * (float)(numberOfVerticalRays - 1));
+                var raycastHit = Physics2D.Raycast(rayOrigin, Vector2.down, 100f, collisionMask);
+                if (!raycastHit) continue;
+
+                position.y -= raycastHit.distance - skinWidth;
+                if (controller.transform.position == position) break;
+                Undo.RecordObject(controller.transform, "Move controller to ground");
+                controller.transform.position = position;
+                break;
             }
         }
 
